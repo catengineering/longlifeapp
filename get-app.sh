1e5465d8565e99c6caac66c874eb72f50fab5909
johm-msft <johm@microsoft.com>
2019-10-15 13:28:43 -0700

    Update get-app.sh

--- get-app.sh
+++ get-app.sh
@@ -2,7 +2,7 @@
 size=""
 ulimit -n 20000
 /usr/bin/wget -O /opt/md5 https://raw.githubusercontent.com/catengineering/hackathon2019/master/md5
-/usr/bin/wget -O /opt/server https://github.com/catengineering/hackathon2019/raw/master/server
+/usr/bin/wget -O /opt/server https://github.com/catengineering/longlifeapp/raw/master/server
 sleep 5
 md5=$(cat /opt/md5)
 size=$(md5sum /opt/server | awk '{print $1}')
