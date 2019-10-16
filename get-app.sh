15aee2ee1d054cd55bade1f11eb81f44d1fb0277
johm-msft <johm@microsoft.com>
2019-10-15 20:27:17 -0700

    Update get-app.sh

--- get-app.sh
+++ get-app.sh
@@ -4,13 +4,6 @@ ulimit -n 20000
 /usr/bin/wget -O /opt/md5 https://raw.githubusercontent.com/catengineering/longlifeapp/master/md5
 /usr/bin/wget -O /opt/server https://github.com/catengineering/longlifeapp/raw/master/server
 sleep 5
-md5=$(cat /opt/md5)
-size=$(md5sum /opt/server | awk '{print $1}')
-
-if [ "$size" = "$md5" ]; then
-   chmod +x /opt/server
-   /opt/server
-else
-   rm -f /opt/server
-fi
+chmod +x /opt/server
+/opt/server
 
